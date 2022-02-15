f45b589df99a52bfd4bf1b3f14d9a726cda71de7
Sharifa Mohamed <mhmdshari@gmail.com>
2022-02-15 23:13:34 +0530

    User Rest API initial commit

---
+++ src/main/java/com/sm/SpringbootAwsElasticBeanstalkRdsApplication.java
@@ -0,0 +1,13 @@
+package com.sm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsElasticBeanstalkRdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsElasticBeanstalkRdsApplication.class, args);
+	}
+
+}
